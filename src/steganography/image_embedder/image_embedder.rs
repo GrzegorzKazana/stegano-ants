a9b35afeb956ac59e09bef4c5743b028d63a32a2
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2021-02-05 19:02:10 +0100

    feat: handle cases when image capacity is larger than embedded data

--- src/steganography/image_embedder/image_embedder.rs
+++ src/steganography/image_embedder/image_embedder.rs
@@ -1,4 +1,4 @@
-use itertools::{EitherOrBoth, Itertools};
+use itertools::Itertools;
 
 use crate::images::image::Pixel;
 use crate::images::pixel_map::PixelMap;
@@ -19,22 +19,27 @@ impl ImageEmbedder {
         (mask_byte as f32 + 1.0).log2().floor() as usize
     }
 
-    fn embed_n_least_significant_bits_in_byte<
-        I: Iterator<Item = Bit> + DoubleEndedIterator + ExactSizeIterator,
-    >(
+    fn embed_n_bits_in_byte<I: Iterator<Item = Bit> + DoubleEndedIterator + ExactSizeIterator>(
         bits_iter: &mut I,
         n_bits: usize,
         transport_byte: Byte,
     ) -> Byte {
-        Data::byte_to_bits_iter(transport_byte)
-            .rev()
-            .zip_longest(bits_iter.take(n_bits).rev())
-            .map(|bits| match bits {
-                EitherOrBoth::Both(_, hidden_bit) => hidden_bit,
-                EitherOrBoth::Left(transport_bit) => transport_bit,
-                EitherOrBoth::Right(hidden_bit) => hidden_bit,
-            })
-            .rev()
+        // postfix is only applicable in cases when we run out of bits to embed while processing the byte
+        // it is important to note that for given input:
+        // n_bits: 5, transport_byte: 0b00000000, bits_iter: 1,1,1 (end of iterator)
+        // we produce output of 0b00011100 instead of 0b00000111
+        let transport_bits_a = Data::byte_to_bits_iter(transport_byte);
+        let transport_bits_b = Data::byte_to_bits_iter(transport_byte);
+
+        let prefix = transport_bits_a.take(8 - n_bits);
+        let infix = bits_iter.take(n_bits);
+
+        let postfix_length = n_bits - infix.len();
+        let postfix = transport_bits_b.rev().take(postfix_length).rev();
+
+        prefix
+            .chain(infix)
+            .chain(postfix)
             .enumerate()
             .map(|(idx, bit)| bit.shift_left(7 - idx).raw())
             .sum()
@@ -47,11 +52,7 @@ impl ImageEmbedder {
     ) -> Byte {
         let n_bits_to_embed = Self::calculate_n_of_bits_to_embed(mask_pixel_channel);
 
-        Self::embed_n_least_significant_bits_in_byte(
-            bits_iter,
-            n_bits_to_embed,
-            transport_pixel_channel,
-        )
+        Self::embed_n_bits_in_byte(bits_iter, n_bits_to_embed, transport_pixel_channel)
     }
 
     fn embed_pixel<I: Iterator<Item = Bit> + DoubleEndedIterator + ExactSizeIterator>(
